cee70f6facd1dfa3feb5da2910989c75bcd3a8ab
T U <tylerletendre98@gmail.com>
2021-12-08 17:57:44 -0500

    added the initial setup for my react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>this is my router</h1>
+      <Link to="/aboutMe">
+        <button>about me</button>
+      </Link>
+      <Routes>
+        <Route path="/aboutMe" element={<AboutMe />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>This is the about me page</h1>
+    </div>
+  );
+}
+
+export default AboutMe;
